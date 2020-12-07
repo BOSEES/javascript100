17c07d206906e4e40808ab0cd83a2fc32c92bdd5
김영일 <rladuddlf3@naver.com>
2020-12-07 23:45:04 +0900

    정리

--- 1-10/10.js
+++ 1-10/10.js
@@ -8,17 +8,36 @@
 //        *******
 //       *********
 
-const level = 5;
+// const level = 5;
 
-for(let i = 1; i <= level; i++){
-  let star = ""
+// for(let i = 1; i <= level; i++){
+//   let star = ""
 
-  for(let k = 1; k <= level - i; k++){
-    star = star + " ";
-  }
+//   for(let k = 1; k <= level - i; k++){
+//     star = star + " ";
+//   }
 
-  for(let j = 1; j <= i * 2 - 1; j++){
-    star = star + "*";
-  }
-  console.log(star);
-}
+//   for(let j = 1; j <= i * 2 - 1; j++){
+//     star = star + "*";
+//   }
+//   console.log(star);
+// }
+
+// ---------------------함수형
+
+// function test(level){
+//   for(let i = 1; i <= level; i++){
+//     let star = ""
+  
+//     for(let k = 1; k <= level - i; k++){
+//       star = star + " ";
+//     }
+  
+//     for(let j = 1; j <= i * 2 - 1; j++){
+//       star = star + "*";
+//     }
+//     console.log(star);
+//   }
+// }
+
+// test(20);
