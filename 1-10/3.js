9029d550124915031f763da910ca8320f7c913c1
김영일 <rladuddlf3@naver.com>
2020-12-07 23:42:34 +0900

    1-10문제 풀기

---
+++ 1-10/3.js
@@ -0,0 +1,6 @@
+// 다음 출력값으로 올바른 것은?
+
+let arr = [100, 200, 300];
+console.log(typeof(arr));
+
+// 정답) object
---
+++ 1-10/9.js
@@ -0,0 +1,17 @@
+// 다음 소스 코드를 완성하여 날짜와 시간을 출력하시오(concat() 메서드를 사용해라)
+
+// 데이터
+let year = "2020";
+let month = "04";
+let day = "26";
+let hour = "11";
+let minute = "34";
+let second = "27";
+
+let result = year.concat("/", month, "/", day, " ", hour, ":", minute, ":", second)//빈칸을 채워주세요
+// let result = year.concat(`/${month}/${day} ${hour}:${minute}:${second}`)//빈칸을 채워주세요
+
+console.log(result);
+
+//출력 2020/04/26 11:34:27
+
